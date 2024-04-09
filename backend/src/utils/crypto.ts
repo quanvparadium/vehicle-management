7544938dc97f799a3dea83ad7b07de5dde1c0436
giaNguyen214 <156935874+giaNguyen214@users.noreply.github.com>
2024-04-09 16:42:20 +0700

    Delete backend directory

--- backend/src/utils/crypto.ts
+++
@@ -1,9 +0,0 @@
-import { createHash } from 'crypto'
-
-export function sha256(content) {
-    return createHash('sha256').update(content).digest('hex')
-}
-
-export function hashPassword(password: string, algorithm: (any) => string = sha256) {
-    return algorithm(password)
-}
