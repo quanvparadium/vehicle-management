2b5b6c5d147170ffdab8dbcf541a2ab69cdb2715
thaihoaho <thaihoa.hn120@gmail.com>
2024-04-10 10:17:59 +0700

    Hi

---
+++ backend/src/constants/messages.ts
@@ -0,0 +1,15 @@
+export const USERS_MESSAGES = {
+    USERNAME_ALREADY_EXIST: 'Username already exists',
+    LOGIN_FAILED: 'Login failed',
+    LOGIN_SUCCESS: 'Login successfully!',
+    LOGOUT_SUCCESS: 'Logout successfully!',
+    REGISTER_FAILED: 'Register failed',
+    REGISTER_SUCCESS: 'Register successfully!',
+    ACCESS_TOKEN_IS_REQUIRED: 'Access token is required',
+    REFRESH_TOKEN_IS_REQUIRED: 'Refresh token is required',
+    VALIDATION_ERROR: 'Validation error',
+    PASSWORD_IS_REQUIRED: 'Password is required',
+    PASSWORD_LENGTH_MUST_BE_FROM_6_TO_50: 'Password length must be between 6 and 50',
+    PASSWORD_MUST_BE_STRONG: 'Password must be strong',
+    CONFIRM_PASSWORD_IS_EQUAL_TO_PASSWORD: 'Password confirmation does not match password'
+}
---
+++ backend/src/middlewares/auth.middlewares.ts
@@ -0,0 +1,52 @@
+import { checkSchema } from 'express-validator'
+import { validate } from '~/utils/validation'
+import { USERS_MESSAGES } from '~/constants/messages'
+import databaseService from '~/services/database.service'
+import { ErrorWithStatus } from '~/models/Errors'
+import HTTPSTATUS from '~/constants/httpStatus'
+
+export const loginValidator = validate(
+    checkSchema({
+        username: {
+            isString: true,
+            notEmpty: true,
+            custom: {
+                options: async (value, { req }) => {
+                    console.log('Value', value)
+                    console.log('Password:', req.body.password)
+                    const user = await databaseService.users.findOne({
+                        username: value,
+                        password: req.body?.password || ''
+                    })
+                    if (user === null) {
+                        throw new ErrorWithStatus({
+                            message: 'Tài khoản không tồn tại trong cơ sở dữ liệu',
+                            status: HTTPSTATUS.BAD_REQUEST
+                        })
+                    }
+                    console.log('Đăng nhập thành công')
+                    req.user = user
+                    return true
+                }
+            }
+        },
+        password: {
+            notEmpty: true,
+            isString: true,
+            isLength: {
+                options: {
+                    min: 6,
+                    max: 50
+                },
+                errorMessage: USERS_MESSAGES.PASSWORD_LENGTH_MUST_BE_FROM_6_TO_50
+            }
+        }
+    })
+)
+
+// Example Validator
+// export const loginValidator = validate(
+//     checkSchema({
+//
+//     })
+// )
