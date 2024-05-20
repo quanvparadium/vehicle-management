ba08af28f1c9585015883401c5fbbb1c170a1b64
thaihoaho <thaihoa.hn120@gmail.com>
2024-05-20 09:44:24 +0700

    get province

--- backend/src/routes/vehicle.routes.ts
+++ backend/src/routes/vehicle.routes.ts
@@ -4,44 +4,13 @@ import { getALLVehicleController, CreateVehicleController, DeleteVehicleControll
 import {  CreateVehicleValidator,UpdateVehicleValidator } from "~/middlewares/vehicle.middlewares";
 
 const vehicleRouter = Router()
-/*decripsion: 'Get all vehicles from database'
-path: /vehicles/
-method: GET
-header: { Authorization: Bearer <access_token> }*/
+
 vehicleRouter.get('/', getALLVehicleController) 
-
-/*decripsion: 'Get all vehicles from database'
-path: /vehicles/
-method: POST
-header: { Authorization: Bearer <access_token> }
-body: { VehicleReqBody } 
-{
-    Vehicle_name: string
-    Vehicle_id: ObjectId
-    Driver_id: ObjectId
-    Driver_name: string
-    type: string
-    model: string
-    number_plate: string
-    frame_number: string
-    chassis_number: string
-    state: string
-    on_trip: boolean
-    fuel_state: number
-    runned_km: number
-    date_recent_maintenance: Date
-    date_next_maintenance: Date
-    current_location: string
-}
-
-*/
-
 vehicleRouter.post('/',CreateVehicleValidator, CreateVehicleController)
 
 vehicleRouter.delete('/:id', DeleteVehicleController)
 vehicleRouter.put('/:id', UpdateVehicleValidator, updateVehicleController);
 vehicleRouter.get('/:id', getVehicleController)
-
 
 
 export default vehicleRouter
